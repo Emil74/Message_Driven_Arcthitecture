713035906d72796c4e3a6751302cb33f82317a8d
Emil74 <emil_nagiyev_98@mail.ru>
2023-03-22 12:22:53 +0300

    Add project files.

---
+++ Message_Driven_Arcthitecture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Message_Driven_Arcthitecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
